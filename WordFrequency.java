bfdcd12c956b44e0e7e04343dff75001e394bd59
TringApps-Nithish <123442599+TringApps-Nithish@users.noreply.github.com>
2023-02-28 07:17:58 +0530

    Update WordFrequency.java

--- WordFrequency.java
+++ WordFrequency.java
@@ -1,36 +1,33 @@
-package as;
+import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Stream;
 
-public class WordFrequency {
+public class F_WordFrequency {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("\n\t\t ----- Frequency Of Word Using HashMap and PriorityQueue -----\n");
-        System.out.printf(" Enter No. of Sentences : ");
-        int n = sc.nextInt();
-        System.out.println();
-        sc.nextLine();
-        String[] input_text = new String[n];
-        for (int i = 0; i < n; i++) {
-            System.out.printf(" Enter Sentence %d : ", i + 1);
-            input_text[i] = sc.nextLine();
-        }
+        String n;
+            try (BufferedReader bfr = new BufferedReader(new FileReader("/home/vijay/Desktop/Tring_Apps Assignment/Data Structure/a.txt"))) 
+            {
+                HashMap<String,Integer> map = new HashMap<>();
+                
+                while ((n = bfr.readLine()) != null) 
+                {
+                    String words[] = n.split(" ");
+                    for (int i = 0; i < words.length; i++) 
+                    {
+                        if(map.containsKey(words[i]))
+                            map.put(words[i],map.get(words[i])+1);
+                        else
+                            map.put(words[i],1);
+                    }
 
-
-        HashMap<String, Integer> Counting = new HashMap<>();
-        for (String lineOfText : input_text) {
-            String[] seperate_words = lineOfText.split("\\s+");
-            for (String word : seperate_words) {
-                Integer count = Counting.get(word);
-                if (count == null) {
-                    count = 0;
+    Stream<Entry<String, Integer>> sorted=  map.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        sorted.forEach(System.out::println);
                 }
-                Counting.put(word, count + 1);
-            }
-        }
-        PriorityQueue<String> sortedWords = new PriorityQueue<>((a, b) -> Counting.get(b) - Counting.get(a));
-        sortedWords.addAll(Counting.keySet());
-
-        for (String word : sortedWords)
-            System.out.println(" Word : "+ word  + "  Frequency = "+ Counting.get(word));
+            } 
+            catch (Exception e) 
+            {
+            System.out.println(e);
+            }    
     }
 }
